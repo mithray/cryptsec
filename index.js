0f105faeea961d19c1e1d42f9f7f37f06a898d0d
mithrayls <mithray@protonmail.com>
2019-09-16 01:58:27 +1200

    Fix require error that results in performing a hashtest instead of encrypting

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let encrypt = require('./lib/hashTest.js')
-let decrypt = require('./lib/decrypt.js')
-let hashTest = require('./lib/hashTest.js')
-let getHash = require('./lib/getHash.js')
-let padString = require('./lib/padString.js')
+const encrypt = require('./lib/encrypt.js')
+const decrypt = require('./lib/decrypt.js')
+const hashTest = require('./lib/hashTest.js')
+const getHash = require('./lib/getHash.js')
+const padString = require('./lib/padString.js')
 
 module.exports = { encrypt, decrypt, hashTest, getHash, padString }
-
 //hashTest()
